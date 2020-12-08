88b60435aef9135a9cfa139e2c04486a54e787ee
shubham1479 <67508771+shubham1479@users.noreply.github.com>
2020-12-08 17:57:12 +0530

    commit

--- assignment1.c
+++ assignment1.c
@@ -1,6 +1,6 @@
 // WAP to cal area and perimeter of rectangle and triangle
-// 2-Dec-2020
-//Name : shubham narayan pandav
+// 8-Dec-2020
+//Name : shubham  pandav
 
 #include <stdio.h>
 
@@ -11,27 +11,34 @@ void main()
     float area_t;
     int peri_t, peri_r, area_r;
 
-    printf("\nEnter the values of Three sides : ");
-    scanf("%d%d%d", &side1, &side2, &side3);
+    printf("\nEnter the values of  side1 : ");
+    scanf("%d",&side1);
 
-    printf("Enter the values of Lenght and breadth :");
-    scanf("%d%d", &len, &breadth);
+    printf("\nEnter the values of  side2 : ");
+    scanf("%d",&side2);
+    printf("\nEnter the values of  side3 : ");
+    scanf("%d",&side3);
+
+    printf("\nEnter the values of Lenght  :");
+    scanf("%d", &len);
+
+    printf("\nEnter the values of Lenght  :");
+    scanf("%d",  &breadth);
     // cal area of rectangle
 
     area_r = len * breadth;
 
     // cal area of triangle
 
-    area_t = side1*side2/2;
-    
+    area_t = side1 * side2 / 2;
+
     // cal perimeter of rectangle
 
-    peri_r = 2*(len+breadth);
+    peri_r = 2 * (len + breadth);
 
     // cal perimeter of triangle
 
-    peri_t = side1+side2+side3;
-
+    peri_t = side1 + side2 + side3;
 
     // display result
 
