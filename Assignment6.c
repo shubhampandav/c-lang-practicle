68681292528d546051a13e8194949de4c576a132
shubham1479 <67508771+shubham1479@users.noreply.github.com>
2021-01-09 11:23:46 +0530

    commit

---
+++ Assignment6.c
@@ -0,0 +1,30 @@
+// program to print reverse a number 
+// 09-01-2021
+
+#include <stdio.h>
+void main()
+{
+    int number, rev = 0, remainder;
+    printf("Enter an number: ");
+    scanf("%d", &number);
+    while (number != 0)
+    {
+        remainder = number % 10;
+        rev = rev * 10 + remainder;
+        number/= 10;
+    }
+    printf("Reversed number = %d", rev);
+}
+ // Algorithm 
+ /**
+ 1. start
+ 2. input number 
+ 3. Initialize rev = 0,  and reminder
+ 4. Loop while num != 0
+     4.1  remainder = number % 10;
+         rev = rev * 10 + remainder;
+         number /= 10;
+
+ 5. disply result 
+ 6. stop 
+ **/
